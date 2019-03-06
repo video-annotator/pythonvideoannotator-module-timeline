696975f1b5650aa7daf5edc6a72c3d314de8b7f6
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-03-06 11:27:20 +0000

    version 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import re
 
 setup(
 	name='Python video annotator - module - timeline',
-	version=0.1,
+	version=0.2,
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
